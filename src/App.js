a793e13820019ef13fe7d40a48b52a8e9f510130
rashid-ab <rashidbuttuog@gmail.com>
2021-03-09 23:32:07 +0500

    'init'

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container Containers-css">
+        <h1>Welcome To</h1>
+        <h1 className="gps">GPS Web</h1>
+      
+        <div className="signForm">
+          <h2 className="signin">Sign In</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
